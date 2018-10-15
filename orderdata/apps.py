52f7c0bff715ddffcf9d99cc1baf3761b1657ee4
Alex Kyungho Park <30945486+konkuest@users.noreply.github.com>
2018-10-15 21:43:42 +0900

    Initial commit

---
+++ orderdata/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrderdataConfig(AppConfig):
+    name = 'orderdata'
---
+++ orderdata/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils import timezone
+
+from django.shortcuts import render
+import json
+import ast
+# Create your models here.
